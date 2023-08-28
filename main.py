61f706078a9e3ba304c2fa61e26d4f8a4dd900bf
notmhx <michaelherrmann111104@gmail.com>
2023-08-28 08:42:09 +0200

    added delete

--- main.py
+++ main.py
@@ -19,3 +19,9 @@ def add():
 def update(index):
     helper.update(index)
     return redirect(url_for("index"))
+
+
+@app.route('/delete')
+def delete(): 
+    helper.delete()
+    return redirect(url_for("index"))
